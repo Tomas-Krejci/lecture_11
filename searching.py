cd005afe0e4aa5179f189504107aa9b028660b0f
Tomas Krejci <226398@vutbr.cz>
2021-04-20 17:18:09 +0200

    linear_search

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,10 +14,33 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    is field not in {"unordered_numbers", "ordered_numbers", "dna_sequence"}
+        return None
+
+    with open(file_path, "r") as json_file:
+        dict = json.load(json_file)
+
+    return dict[field]
+
+def linear_search(sequence, key)
+    listx = []
+    count = 0
+
+    for idx in range(len(sequence))
+        if sequence[idx] == key:
+            listx.append(idx)
+            count += 1
+
+    return {
+        "positions": listx,
+        "count":count
+    }
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
-
+    result = linear_search(sequential_data, 5)
+    print(result)
 if __name__ == '__main__':
     main()
